21710682ee580bad43e964a1a172fc4c8357598c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-12-26 23:32:45 +0530

    feat: add enma constants

---
+++ src/utils/constants.go
@@ -0,0 +1,7 @@
+package utils
+
+const (
+	ACCEPT_ENCODING_HEADER string = "gzip, deflate, br"
+	USER_AGENT_HEADER      string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+	ACCEPT_HEADER          string = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+)
